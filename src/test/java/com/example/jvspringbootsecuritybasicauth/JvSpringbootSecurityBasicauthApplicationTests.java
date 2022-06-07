4cfce39d36a5ad249a9a5b08829d21906b57fda8
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2022-06-07 15:57:51 +0300

    init commit

---
+++ src/test/java/com/example/jvspringbootsecuritybasicauth/JvSpringbootSecurityBasicauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvspringbootsecuritybasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvSpringbootSecurityBasicauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
